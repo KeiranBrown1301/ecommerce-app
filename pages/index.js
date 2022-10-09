54f9994ab690139fed7369c0d7367b8319e80cd3
Keiran Brown <keiranbrown1301@gmail.com>
2022-10-09 23:42:57 +0900

    styles added to homepage div

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@ function Home() {
   return (
     <>
       HeroBanner
-      <div>
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["Product1", "Product2"].map((product) => product)}</div>
+      <div className="products-container">
+        {["Product1", "Product2"].map((product) => product)}
+      </div>
       Footer
     </>
   );
