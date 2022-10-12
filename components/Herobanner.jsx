9d2316cd9272c14db155fb4ebb7383140fb93398
Keiran Brown <keiranbrown1301@gmail.com>
2022-10-12 18:38:30 +0900

    herobanner component all data has been added.

--- components/Herobanner.jsx
+++ components/Herobanner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import {} from "module";
+import { urlFor } from "../lib/client";
 
 function HeroBanner({ heroBanner }) {
   return (
@@ -9,14 +9,18 @@ function HeroBanner({ heroBanner }) {
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src="" alt="headphones" className="hero-banner-image" />
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
         <div>
-          <Link href="/product/ID">
-            <button type="button">BUTTON TEXT</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
