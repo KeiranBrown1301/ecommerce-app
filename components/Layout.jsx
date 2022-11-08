527119af3599180fa7bf6ce3d89ee351dfb7ca91
Keiran Brown <keiranbrown1301@gmail.com>
2022-11-08 21:53:30 +0900

    beginning to build the layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import Head from "next/head";
+import Navbar from "./Navbar";
 
 function Layout() {
-  return <div>Layout</div>;
+  return (
+    <div className="layout">
+      <Head>
+        <title>Electronics Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">EMPTY</main>
+    </div>
+  );
 }
 
 export default Layout;
