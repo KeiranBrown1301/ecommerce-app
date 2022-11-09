d9e995c40ade890372bd00a7fe26c4dd163fe21a
Keiran Brown <keiranbrown1301@gmail.com>
2022-11-09 19:41:38 +0900

    getStaticProps implementd in slug file

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,32 @@
 import React from "react";
+import { client, urlFor } from "../../lib/client";
 
 function ProductDetails() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export const getStaticProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {
+      products,
+      bannerData,
+    },
+  };
+};
 
 export default ProductDetails;
