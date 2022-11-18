7f73236c02934748bb1c5a5b674c7be2c00d18b1
Keiran Brown <keiranbrown1301@gmail.com>
2022-11-18 17:13:58 +0900

    product page is looking great, all items when hovered switch, routes work and also may like is functioning well

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -17,7 +17,10 @@ function ProductDetails({ product, products }) {
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[index])} />
+            <img
+              className="product-detail-image"
+              src={urlFor(image && image[index])}
+            />
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
