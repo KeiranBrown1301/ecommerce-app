28385d057c684b13428d62ff267a8b74ecd264e2
Keiran Brown <keiranbrown1301@gmail.com>
2022-11-21 16:52:17 +0900

    required states all created.

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,2 +1,16 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+function StateContext({ children }) {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuantity, setTotalQuantity] = useState();
+  const [qty, setQty] = useState(1);
+
+  return <Context.Provider>{children}</Context.Provider>;
+}
+
+export default StateContext;
